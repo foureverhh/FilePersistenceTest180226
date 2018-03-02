01bb011cd4ab0433ec22f4e5597f5438b78517ae
foureverhh <foureverhh@gmail.com>
2018-03-02 12:32:01 +0100

    Add title and subtile to an actionbar though toolbar

--- ToolbarLearningYoutube180228/app/src/main/java/com/nackademin/foureverhh/toolbarlearningyoutube180228/MainActivity.java
+++ ToolbarLearningYoutube180228/app/src/main/java/com/nackademin/foureverhh/toolbarlearningyoutube180228/MainActivity.java
@@ -1,8 +1,12 @@
 package com.nackademin.foureverhh.toolbarlearningyoutube180228;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +17,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = findViewById(R.id.app_bar); //Initialise the Toolbar object
-        setSupportActionBar(toolbar);         //Set the toolbar as app bar
+        //Initialise the Toolbar object
+        toolbar = findViewById(R.id.app_bar);
+        //Set the toolbar as app bar
+        setSupportActionBar(toolbar);
 
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setTitle("Welcome");
+        actionbar.setSubtitle("all");
+
+        /*works
+        getSupportActionBar().setTitle("Welcome");
+        toolbar.setSubtitle("all");
+
+        getSupportActionBar().setTitle("Welcome");
+        getSupportActionBar().setSubtitle("all");
+        */
+
+        /* Does not work
+        getSupportActionBar().setTitle("Welcome");
+        toolbar.setSubtitle("all");
+
+        toolbar.setTitle("Welcome");
+        getSupportActionBar().setSubtitle("all");
+        */
+
+     /*   //Make the ActionBar, which is toolbar in this case , can redirect back to the home page
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);*/
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
