85c114432dac2cb12c581dba2afbcee05d05f520
foureverhh <foureverhh@gmail.com>
2018-03-27 15:40:58 +0200

    Get lastLocation, going to learn location updates and try to draw polyline on map

--- LocationPractiseYoutube180326/app/src/main/java/com/nackademin/foureverhh/locationpractiseyoutube180326/MainActivity.java
+++ LocationPractiseYoutube180326/app/src/main/java/com/nackademin/foureverhh/locationpractiseyoutube180326/MainActivity.java
@@ -1,5 +1,9 @@
 package com.nackademin.foureverhh.locationpractiseyoutube180326;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,11 +11,17 @@ import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView latitudeText,longitudeText,altitudeText,speedText,accuracyText,sensorType,updatesOnOff;
-    ToggleButton switchGpsBalanced,locationOnOff;
+    TextView latitudeText, longitudeText, altitudeText, speedText, accuracyText, sensorType, updatesOnOff;
+    ToggleButton switchGpsBalanced, locationOnOff;
+    private static final int MY_PERMISSION_REQUEST_FINE_LOCATION = 101;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +38,14 @@ public class MainActivity extends AppCompatActivity {
         switchGpsBalanced = findViewById(R.id.tbGps_Balanced);
         locationOnOff = findViewById(R.id.tvLocationOnOff);
 
+
         switchGpsBalanced.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(switchGpsBalanced.isChecked()){
+                if (switchGpsBalanced.isChecked()) {
                     //Using GPS
                     sensorType.setText("GPS");
-                }else{
+                } else {
                     //Using balanced power accuracy
                     sensorType.setText("Cell Tower and WiFi");
                 }
@@ -44,10 +55,10 @@ public class MainActivity extends AppCompatActivity {
         locationOnOff.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(locationOnOff.isChecked()){
+                if (locationOnOff.isChecked()) {
                     //Location updates on
                     updatesOnOff.setText("On");
-                }else {
+                } else {
                     //Location updates off
                     updatesOnOff.setText("Off");
                 }
@@ -55,7 +66,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        latitudeText.setText(String.valueOf(location.getLatitude()));
+                        longitudeText.setText(String.valueOf(location.getLongitude()));
+                        accuracyText.setText(String.valueOf(location.getAccuracy()));
+                        if (location.hasAltitude()) {
+                            altitudeText.setText(String.valueOf(location.getAltitude()));
+                        } else {
+                            latitudeText.setText("No altitude available");
+                        }
+                        if (location.hasSpeed()) {
+                            speedText.setText(String.valueOf(location.getSpeed()) + "/Sec");
+                        } else {
+                            speedText.setText("No speed available");
+                        }
 
+                    }
+                }
+            });
+        }else{
+            //Request permissions
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},MY_PERMISSION_REQUEST_FINE_LOCATION);
+        }
 
     }
 }
