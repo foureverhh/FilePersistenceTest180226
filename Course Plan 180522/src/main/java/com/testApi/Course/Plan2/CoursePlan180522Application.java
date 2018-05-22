2b9321a26cd6c9f5c90bc40ae8f68e6453523e96
foureverhh <foureverhh@gmail.com>
2018-05-22 14:44:16 +0200

    test spring boot

---
+++ Course Plan 180522/src/main/java/com/testApi/Course/Plan2/CoursePlan180522Application.java
@@ -0,0 +1,12 @@
+package com.testApi.Course.Plan2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePlan180522Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursePlan180522Application.class, args);
+	}
+}
